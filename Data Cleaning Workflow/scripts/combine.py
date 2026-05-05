c07ead6f0068c378e9ecfbc9aa9a4244f0016002
Renee Huang <reneeh3@illinois.edu>
2026-05-05 10:17:10 -0500

    Update combine.py

--- Data Cleaning Workflow/scripts/combine.py
+++ Data Cleaning Workflow/scripts/combine.py
@@ -1,20 +1,11 @@
 import pandas as pd
-import os
 
-os.makedirs("results", exist_ok=True)
+moma = pd.read_csv(snakemake.input.moma)
+met = pd.read_csv(snakemake.input.met)
 
-# Load cleaned datasets
-moma = pd.read_csv("results/moma_snakefile_cleaned.csv")
-met = pd.read_csv("results/met_snakefile_cleaned.csv")
-
-# Add source labels
 moma["source"] = "MoMA"
 met["source"] = "MET"
 
-# Combine
 combined = pd.concat([moma, met], ignore_index=True)
-
-# Save
-combined.to_csv("results/combined_moma_met.csv", index=False)
-
+combined.to_csv(snakemake.output[0], index=False)
 print("Combined dataset created")
