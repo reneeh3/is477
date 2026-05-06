ae5da86df101d52bb3c21c9dd8d2a884fd4a6fae
yewonkim87 <yewonwk2@illinois.edu>
2026-05-05 22:16:35 -0500

    Refactor combine.py for dataset processing
    Updated file paths for input and output CSVs, added directory creation for results, and filtered columns for final datasets.

--- Snakemake_Workflow/scripts/combine.py
+++ Snakemake_Workflow/scripts/combine.py
@@ -1,11 +1,29 @@
 import pandas as pd
+import os
 
-moma = pd.read_csv(snakemake.input.moma)
-met = pd.read_csv(snakemake.input.met)
+os.makedirs("results", exist_ok=True)
+
+moma = pd.read_csv("results/final_moma.csv", low_memory=False)
+met = pd.read_csv("results/final_met.csv", low_memory=False)
+
+columns = [
+    "title",
+    "artist_name",
+    "artist_birthyear",
+    "artist_deathyear",
+    "nationality_clean"
+]
+
+moma = moma[columns].copy()
+met = met[columns].copy()
 
 moma["source"] = "MoMA"
 met["source"] = "MET"
 
 combined = pd.concat([moma, met], ignore_index=True)
-combined.to_csv(snakemake.output[0], index=False)
+
+combined.to_csv("results/combined_moma_met.csv", index=False)
+
 print("Combined dataset created")
+print(combined.shape)
+print(combined["source"].value_counts())
